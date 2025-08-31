19b5d69a761277a973c91acb0c5a7661504b993f
Aesistril <ae@aesistril.com>
2025-08-31 21:39:50 +0300

    license

--- src/creds.c
+++ src/creds.c
@@ -1,4 +1,16 @@
-// creds.c
+// Kilmister Client -- A lemmy client for GTK desktops and mobile devices
+// Copyright (C) 2025-2026
+
+// This file is part of Kilmister Client.
+// Kilmister Client is free software: you can redistribute it and/or modify it under the terms of 
+// the GNU General Public License as published by the Free Software Foundation, either version 
+// 3 of the License, or (at your option) any later version.
+// Kilmister Client is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with Kilmister Client. 
+// If not, see <https://www.gnu.org/licenses/>. 
+
 #include "api/request.h"
 #include "import_ui.h"
 #include <glib.h>
